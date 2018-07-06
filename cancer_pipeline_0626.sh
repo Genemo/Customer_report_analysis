6de7a4f873a1044d7d9754ba165f8439b4b1dcc4
annlyuan <37343354+annlyuan@users.noreply.github.com>
2018-07-05 18:12:00 -0700

    Update cancer_pipeline_0626.sh

--- cancer_pipeline_0626.sh
+++ cancer_pipeline_0626.sh
@@ -15,34 +15,38 @@
 project_path=/Users/annlyuan2018/Downloads/test
 sample_path=/Users/annlyuan2018/Downloads/exp_sample
 reference_path=/Users/annlyuan2018/Downloads/cancer_reference
-
+code_path=/Users/annlyuan2018/Downloads/R_code
 
 for j in $(cd $sample_path;ls *.txt); do
 
-	Name=$(basename $sample_path/$j .txt)
+	Nam=$(basename $sample_path/$j .txt)
 	 
-	 mkdir $project_path/$Name
+	mkdir $project_path/$Nam
+	 
+	awk '{print $1,$6,$7}' $sample_path/$j|tail -n +3 > $project_path/$Nam/union.txt
+
+        Rscript $code_path/counts_to_TPM0.R $project_path/$Nam/union.txt $project_path/$Nam/unionTPM.txt
   
    for i in $(cd $reference_path;ls *.xml); do
 
    	 Cancer=$(basename $reference_path/$i .xml)
 
-     mkdir $project_path/$Name/$Cancer
+         mkdir $project_path/$Nam/$Cancer
 
-     awk '$2>10 {print $1}' $sample_path/$j> $project_path/$Name/$Cancer/exp1.txt
+         awk '$2>10 {print $1}' $sample_path/$j> $project_path/$Nam/$Cancer/exp1.txt
 
-     grep "<identifier" $reference_path/$i | awk '{print $2}' > $project_path/$Name/$Cancer/$Cancer.txt
-     sed -i '' 's/^.\{4\}//g' $project_path/$Name/$Cancer/$Cancer.txt
-     sed -i '' 's/.\{1\}$//g' $project_path/$Name/$Cancer/$Cancer.txt
+         grep "<identifier" $reference_path/$i | awk '{print $2}' > $project_path/$Nam/$Cancer/$Cancer.txt
+         sed -i '' 's/^.\{4\}//g' $project_path/$Nam/$Cancer/$Cancer.txt
+         sed -i '' 's/.\{1\}$//g' $project_path/$Nam/$Cancer/$Cancer.txt
 
-     grep -F -f $project_path/$Name/$Cancer/$Cancer.txt $project_path/$Name/$Cancer/exp.txt > $project_path/$Name/$Cancer/same_$Cancer.txt
-     wc -l $project_path/$Name/$Cancer/$Cancer.txt $project_path/$Name/$Cancer/exp.txt $project_path/$Name/$Cancer/same_$Cancer.txt > $project_path/$Name/$Cancer/results_$Cancer.txt
+         grep -F -f $project_path/$Nam/$Cancer/$Cancer.txt $project_path/$Nam/$Cancer/exp.txt > $project_path/$Nam/$Cancer/same_$Cancer.txt
+         wc -l $project_path/$Nam/$Cancer/$Cancer.txt $project_path/$Nam/$Cancer/exp.txt $project_path/$Nam/$Cancer/same_$Cancer.txt > $project_path/$Nam/$Cancer/results_$Cancer.txt
      
-     awk '{print $1}' $project_path/$Name/$Cancer/results_$Cancer.txt | xargs |awk -v t=$Cancer '{print t,$1,$2,$3}'>> $project_path/total_$Name.txt
+         awk '{print $1}' $project_path/$Nam/$Cancer/results_$Cancer.txt | xargs |awk -v t=$Cancer '{print t,$1,$2,$3}'>> $project_path/total_$Nam.txt
 
    done
 
-     #awk -v r=$(seq 1 $(ls $reference_path/*.xml|wc -l)) '{print r}' $project_path/total_$Name.txt>> $project_path/total_$Name.txt
+     #awk -v r=$(seq 1 $(ls $reference_path/*.xml|wc -l)) '{print r}' $project_path/total_$Nam.txt>> $project_path/total_$Nam.txt
 
 done
 
